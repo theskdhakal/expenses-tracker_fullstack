102e9f4b09b2bf1a6818fe5a933b9fba60bd8565
Shiva Dhakal <theskdhakal@gmail.com>
2023-11-24 12:01:37 +1100

    db deployed in atlas

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import "dotenv/config";
 import cors from "cors";
 const app = express();
 const PORT = 8000;
+import path from "path";
 
 // db connect
 import { connectDB } from "./src/config/dbConfig.js";
@@ -19,10 +21,17 @@ import { authMiddleware } from "./src/middlewares/authMiddleware.js";
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/transaction", authMiddleware, transactionRouter);
 
+//path to server static code made from npm run build
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 // server side rendering
 app.use("/", (req, res, next) => {
   try {
-    res.send("<h1>Coming soon...</h1>");
+    // res.send("<h1>Coming soon...</h1>");
+
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
   } catch (error) {
     next;
   }
--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   try {
-    const MONGO_CLIENT = "mongodb://127.0.0.1/expenses_tracker";
-    const conn = mongoose.connect(MONGO_CLIENT);
+    const conn = mongoose.connect(process.env.MONGO_CLIENT);
 
     if (conn) {
       console.log("MongoDB connected");
